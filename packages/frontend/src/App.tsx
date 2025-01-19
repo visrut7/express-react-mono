82e3df51a9756efbd8882acf066fbac8a8ebbfc3
visrut <visrutnavadiya111@gmail.com>
2025-01-19 15:05:49 +0530

    add tailwind css

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
       },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
   },
 });
